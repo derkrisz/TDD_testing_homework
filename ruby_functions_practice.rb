856949513a64c54bf46585942f6669d64744f558
derkrisz <krisztian.dergmail.com>
2017-10-31 16:36:25 +0000

    length of string test passes

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,19 +1,23 @@
-# def return_10
-#   return 10
-# end
+def return_10
+  return 10
+end
 
-# def add(num1, num2)
-#   return 1+2
-# end
+def add(num1, num2)
+  return 1+2
+end
 
-# def subtract(num1, num2)
-#   return 10-5
-# end
+def subtract(num1, num2)
+  return 10-5
+end
 
-# def multiply(num1, num2)
-#   return 4 * 2
-# end
+def multiply(num1, num2)
+  return 4 * 2
+end
 
 def divide(num1, num2)
   return 10/2
+end
+
+def length_of_string(str)
+  return str.length
 end
